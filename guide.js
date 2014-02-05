cce477974783395e346623fc3e53f4e54096add0
Shashank Teotia <shashankteotia@gmail.com>
2014-02-05 21:19:57 +0530

    added a guide to provide feedback

--- guide.js
+++ guide.js
@@ -1,1 +1,20 @@
+var mediator = require('./mediator'),
+    router = require('./router'),
+    localizer = require('./localizer');
+
+mediator.pubsub.on('newOrientation',function(message){
+    //check if it matches requiredOrientation within a range
+    //and inform
+});
+
+mediator.pubsub.on('waypointReached',function(message){
+
+});
+
+mediator.pubsub.on('destinationReached',function(message){
+
+});
+
+
+
 
