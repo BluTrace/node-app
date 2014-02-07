31facd4774307c5a94b0ddbf88ab744f60be718a
Paras <narangparas@gmail.com>
2014-02-07 06:48:03 +0000

    making sensorTag service discoverey async

--- sensor.js
+++ sensor.js
@@ -1,3 +1,5 @@
+var util = require('util');
+var async = require('async');
 var SensorTag = require('sensortag');
 
 var startSensing = function() {
@@ -5,24 +7,72 @@ var startSensing = function() {
         console.log('discovered');
         sensorTag.on('disconnect', function() {
             console.log('disconnected!');
+            process.exit(0);
         });
-        sensorTag.connect(function(){
-            console.log('connected!');
-            sensorTag.enableMagnetometer(function(){
-                console.log('enabling magnetometer');
-            });
-            sensorTag.on('magnetometerChange', function(x, y, z){
-                console.log('----> ('+x+','+y+','+z+')');
-            });
-            sensorTag.on('simpleKeyChange', function(left, right) {
-                if (left) {
-                    console.log('Left Key Pressed');
-                } else if (right) {
-                    console.log('Right Key Pressed');
-                }
-            });
-        });
+        async.series([
+            function(callback) {
+                console.log('connect');
+                sensorTag.connect(callback);
+            },
+            function(callback) {
+                console.log('discoverServicesAndCharacteristics');
+                sensorTag.discoverServicesAndCharacteristics(callback);
+            },
+            function(callback) {
+                console.log('enableMagnetometer');
+                sensorTag.enableMagnetometer(callback);
+            },
+            function(callback) {
+                console.log('enableGyroscope');
+                sensorTag.enableGyroscope(callback);
+            },
+            function(callback) {
+                setTimeout(callback, 2000);
+            },
+            function(callback) {
+                sensorTag.on('magnetometerChange', function(x, y, z) {
+                    console.log('\tx = %d μT', x.toFixed(4));
+                    console.log('\ty = %d μT', y.toFixed(4));
+                    console.log('\tz = %d μT', z.toFixed(4));
+                });
+                sensorTag.notifyMagnetometer(function() {
+                    console.log('M notified!');
+                 
+                });
+            callback();
+            },
+            function(callback){
+                sensorTag.on('gyroscopeChange', function(x, y, z) {
+                console.log('\tx = %d °/s', x.toFixed(4));
+                console.log('\ty = %d °/s', y.toFixed(4));
+                console.log('\tz = %d °/s', z.toFixed(4));
+                });
+                sensorTag.notifyGyroscope(function() {
+                    console.log('G notified!');
+                });
+            callback();
+            },
+            function(callback) {
+                console.log('readSimpleRead');
+                sensorTag.on('simpleKeyChange', function(left, right) {
+                    console.log('left: ' + left);
+                    console.log('right: ' + right);
+
+                    if (left && right) {
+                        sensorTag.notifySimpleKey(callback);
+                    }
+                });
+
+                sensorTag.notifySimpleKey(function() {
+                 
+                });
+            callback();
+            }
+            ]);
+
     });
 }
 
 module.exports.startSensing = startSensing;
+
+startSensing();
